16b9cc0da6be412294f66a884aebb9a6e2ccfd83
ALFLM <aleffe2323@gmail.com>
2024-12-02 18:07:52 +0100

    Actualización con nuevos cambios de visualización

--- valoracion_nacional.js
+++ valoracion_nacional.js
@@ -43,16 +43,13 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
   // Función para iniciar sesión con Google
-  async function login() {
+  document.getElementById("loginBtn").addEventListener("click", async () => {
     try {
       await signInWithPopup(auth, provider);
     } catch (error) {
       console.error("Error de autenticación: ", error);
-      alert("Hubo un error al intentar registrarte. Intenta nuevamente.");
     }
-  }
-
-  document.getElementById("loginBtn").addEventListener("click", login);
+  });
 
   // Función para cerrar sesión
   document.getElementById("logoutBtn").addEventListener("click", async () => {
